9cbb736262721204efba8c8992bab8c8b30e906f
Inge-dev <ingelewerentz@gmail.com>
2025-03-08 17:19:51 -0800

    Updated index.js
    Create the Express code to route all of the endpoints

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express'),
 
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    name: "Tracy",
+    favoriteMovies: []
+  },
+  {
+    id: 2,
+    name: "Darren",
+    favoriteMovies: []
+  }
+]
+
 const topMovies = [
   {
     title: 'The Blindside'
@@ -39,13 +52,49 @@ const topMovies = [
 app.use(express.static('public'))
 app.use(morgan('common'));
 
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie app!');
-});
-
+//GET
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+//GET
+app.get('/movies/:category/:value', (req, res) => {
+  const { category, value } = req.params;
+
+  if (category === 'title') {
+    res.send(`Successful GET request returning data on the movie titled: ${value}`);
+  } else if (category === 'genre') {
+    res.send(`Successful GET request returning data on movies in the ${value} genre`);
+  } else if (category === 'director') {
+    res.send(`Successful GET request returning data on movies directed by ${value}`);
+  } else {
+    res.status(400).send('Invalid category. Please use "title", "genre", or "director".');
+  }
+});
+
+//CREATE
+app.post('/users', (req, res) => {
+  res.send('User registered successfully');
+});
+
+//UPDATE
+app.put('/users/:id', (req, res) => {
+  res.send('User info was successfully updated');
+});
+
+//CREATE
+app.post('/users/:id/:movieTitle', (req, res) => {
+  res.send('Movie successfully added');
+});
+
+//DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  res.send('Movie successfully removed');
+});
+
+//DELETE
+app.delete('/users/:id', (req, res) => {
+  res.send('User email successfully removed');
 });
 
 const bodyParser = require('body-parser'),
